579285a1dce39560522da9191329014bff504607
Bhavik Patel <140006006+bp0609@users.noreply.github.com>
2023-07-27 00:35:28 +0530

    Add files via upload

---
+++ Day11/Day_11_1.py
@@ -0,0 +1,70 @@
+with open("input_day_11.txt","r") as file:
+    content=file.read()
+lst=content.split("\n\n")
+# print(lst[7],"DONE")
+items=[]
+operations=[]
+operation_nums=[]
+tests=[]
+t_case=[]
+f_case=[]
+iterations=[0]*len(lst)
+def addition(old , num):
+    return old+num
+def multiply(old,num):
+    return old*num
+
+def square(old):
+    return old*old
+
+for i in lst:
+    lst1=i.split("\n ")
+    divisible_num=int(lst1[3].split("by ")[1])
+    t_monkey=int(lst1[4].split("monkey ")[1])
+    f_monkey=int(lst1[5].split("monkey ")[1])
+    item=list(map(int,lst1[1].split(": ")[1].split(",")))    # it is a list
+    if lst1[2].split("old ")[1].split(" ")[1]=="old":
+        operation_num=0
+        operation="sq"
+    
+    else:    
+        operation_num=int(lst1[2].split("old ")[1].split(" ")[1])
+        operation=lst1[2].split("old ")[1].split(" ")[0]
+    
+#--------------Storing Data------------------------------------->>>>    
+    items.append(item)
+    operations.append(operation)
+    operation_nums.append(operation_num)    
+    tests.append(divisible_num)
+    t_case.append(t_monkey)
+    f_case.append(f_monkey)
+
+
+# print(items)
+# print(operation_nums)
+# print(operations)
+# print(tests)
+# print(t_case)
+# print(f_case)
+for i in range(10000):
+    for j in range(len(lst)):
+        iterations[j]=len(items[j])+iterations[j]
+        for item in range(len(items[j])):
+            if not items[j]:
+                break
+            k=items[j].pop(0)
+            if operations[j]=="*":
+                new=multiply(k,operation_nums[j])//3
+            elif operations[j]=="sq":
+                new=square(k)//3
+            else:
+                new = addition(k,operation_nums[j])//3
+                
+            
+            if (new%tests[j])==0:
+                items[t_case[j]].append(new)
+                
+            else:
+                items[f_case[j]].append(new)
+            
+print(iterations)
